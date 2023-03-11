eb16cf02e46ce0b163e3291bd23e038791c43775
Taehyun Kim <k1m743hyun@icloud.com>
2023-03-11 13:04:02 +0000

    Update StringEquals.java

---
+++ StringEquals.java
@@ -0,0 +1,9 @@
+class StringEquals {
+  public static void main(String args[]) {
+    //String input = "";
+    String input = null;
+
+    System.out.println("a".equals(input));
+    System.out.println(input.equals("a"));
+  }
+}
