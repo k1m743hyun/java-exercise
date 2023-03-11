7318d607a8860d9a2d11e684781a1adf08721522
Taehyun Kim <k1m743hyun@icloud.com>
2023-03-11 13:04:30 +0000

    Create IntegerToString.java

---
+++ IntegerToString.java
@@ -0,0 +1,9 @@
+class IntegerToString {
+  public static void main(String args[]) {
+    //Integer i = 0;
+    Integer i = null;
+    
+    System.out.println(String.valueOf(i));
+    System.out.println(Integer.toString(i));
+  }
+}
