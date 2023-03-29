c867586851f706749016a9540640a48cf17f1533
Taehyun Kim <k1m743hyun@icloud.com>
2023-03-29 09:55:04 +0900

    Update src/TryWithResources.java

--- src/TryWithResources.java
+++ src/TryWithResources.java
@@ -8,17 +8,10 @@ public class TryWithResources {
 
     public static void main(String[] args) {
 
-        Scanner scanner = null;
-
-        try {
-            scanner = new Scanner(new File("input.txt"));
+        try (Scanner scanner = new Scanner(new File("input.txt"))) {
             System.out.println(scanner.nextLine());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
         }
     }
 }
