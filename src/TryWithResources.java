6f9af9f2f1ec78d01c886d71a2238fba1d90f5d3
Taehyun Kim <k1m743hyun@icloud.com>
2023-03-29 09:57:57 +0900

    Update src/TryWithResources.java

--- src/TryWithResources.java
+++ src/TryWithResources.java
@@ -2,14 +2,17 @@ package src;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class TryWithResources {
 
     public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(new File("input.txt"))) {
+        try (Scanner scanner = new Scanner(new File("input.txt"));
+            PrintWriter printWriter = new PrintWriter(new File("output.txt"))) {
             System.out.println(scanner.nextLine());
+            printWriter.println("End");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
