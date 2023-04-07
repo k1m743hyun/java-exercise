e9405f74aac70d151911aeecedcee66f37bb3579
Taehyun Kim <k1m743hyun@icloud.com>
2023-04-07 09:06:07 +0000

    Create src/DifferenceBetweenCallByValueAndCallByReference.java

---
+++ src/DifferenceBetweenCallByValueAndCallByReference.java
@@ -0,0 +1,11 @@
+package src;
+
+class DifferenceBetweenCallByValueAndCallByReference {
+  
+  public static void main(String args[]) {
+
+    int a = 10;
+    int b = 10;
+    System.out.println("primitive type: " + (a == b));
+  }
+}
