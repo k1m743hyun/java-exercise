c4c4dd4df06f0e1b865358ca137ef275e6083aeb
Taehyun Kim <k1m743hyun@icloud.com>
2023-04-14 04:22:49 +0000

    Update src/Split.java

--- src/Split.java
+++ src/Split.java
@@ -1,17 +1,21 @@
 package src;
 
-import java.util.StringTokenizer;
-
 class Split {
 
   public static void main(String[] args) {
-    String[] arr = new String[]{"", "/", "a/", "/b", "a/b"};
+    String[] arr = new String[]{"", "/", "a/", "/b", "a/b", "a/b/c", "a/b/c/", "a/b/c/d"};
     for (String s : arr) {
+      System.out.println(s);
+      
       // 공백 제거
       System.out.println(s.split("/").length);
 
+      System.out.println(s.split("/", 0).length);
+
       // 공백 유지
       System.out.println(s.split("/", -1).length);
+      
+      System.out.println(s.split("/", -2).length);
       System.out.println("-----");
     }
   }
