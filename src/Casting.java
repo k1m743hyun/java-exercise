0255a73d27cbbdd2814b12bb7d6503372f0a8e9a
Taehyun Kim <k1m743hyun@icloud.com>
2023-04-19 06:30:48 +0000

    Create src/Casting.java

---
+++ src/Casting.java
@@ -0,0 +1,12 @@
+package src;
+
+
+class Casting {
+
+  public static void main(String[] args) {
+
+    Object time = null;
+    System.out.println((Long) time);
+    System.out.println((long) time);
+  }
+}
