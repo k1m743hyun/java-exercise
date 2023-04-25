5b01394d4437e36f69757d876a2816a8a73971eb
Taehyun Kim <k1m743hyun@icloud.com>
2023-04-25 02:52:29 +0000

    Update src/Static.java

--- src/Static.java
+++ src/Static.java
@@ -1,23 +1,23 @@
+package src;
+
 public class Static {
+  
   public static void main(String[] args) {
     
+    Person person1 = new Person();
+    Person person2 = new Person();
+
+    System.out.println(person1.getName());
+    System.out.println(person2.getName());
   }
 }
 
 public class Person {
 
-  private String name = "Taehyun Kim";
+  //private String name = "Taehyun Kim";
+  private static String name = "Hyuntae Kim";
 
-  public void printName() {
-    System.out.println(this.name);
-  }
-}
-
-public class StaticPerson {
-
-  public static final String name = "Taehyun Kim";
-
-  public static void printName() {
-    System.out.println(this.name);
+  public String getName() {
+    return this.name;
   }
 }
