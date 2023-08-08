de927dcfc4c30de83454e7e5d8b00aab4e2e6ed3
Taehyun Kim <k1m743hyun@icloud.com>
2023-08-08 05:17:39 +0000

    Update src/CalculateWeekOfMonth.java

--- src/CalculateWeekOfMonth.java
+++ src/CalculateWeekOfMonth.java
@@ -1,18 +1,21 @@
 package src;
 
 import java.util.Calendar;
+import java.util.TimeZone;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
 class CalculateWeekOfMonth {
 
   public static void main(String[] args) {
+    
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
     try {
-    String date = "2023-06-26";
-    Calendar calendar = Calendar.getInstance();
-    calendar.setTime(new SimpleDateFormat("yyyy-mm-dd").parse(date));
-    // calendar.setFirstDayOfWeek(Calendar.MONDAY);
-    System.out.println(calendar.get(Calendar.WEEK_OF_MONTH));
+      String date = "2023-07-10";
+      Calendar calendar = Calendar.getInstance();
+      calendar.setTime(dateFormat.parse(date));
+      // calendar.setFirstDayOfWeek(Calendar.MONDAY);
+      System.out.println(calendar.get(Calendar.WEEK_OF_MONTH));
     } catch (ParseException e) {
       throw new RuntimeException(e);
     }
